4ff39f7d5436dca342578d1b81cd3ae17321d1fe
Suli Xi <sxi@yext.com>
2023-04-17 23:50:11 -0400

    Move js to tsx file. Clean up all unused files.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,27 +1,45 @@
 import * as React from "react";
 import { Address } from "../types/Address";
-import Cta from "./Cta";
 
 export interface BannerProps {
   name?: string;
   address?: Address;
 }
 
+const zeroPad = (num, places) => String(num).padStart(places, '0')
+
+function checkTimestamp(name) {
+  let now = new Date();
+  let updateTime = Date.parse(name.replace("Updated on ",""))
+  let elapsedTime = (now.getTime()-updateTime)/1000
+  let result = "GOOD"
+  if (elapsedTime > 30 * 60)
+    result = "BAD"
+  let elapsedSecond = Math.floor(elapsedTime % 60)
+  elapsedTime = Math.floor(elapsedTime / 60)
+  let elapsedMinute = elapsedTime % 60
+  let elapsedHour = Math.floor(elapsedTime / 60)
+  let elapse = zeroPad(elapsedHour,2) + ":" + zeroPad(elapsedMinute,2) + ":" + zeroPad(elapsedSecond,2) + " Seconds ago";
+  return {'elapse':elapse, 'result':result}
+}
 
 const Banner = (props: BannerProps) => {
   const { name, address } = props;
-
+  let {elapse, result} = checkTimestamp(name)
+  let color = "green"
+  if (result == "BAD")
+    color = "red"
   return (
     <>
         <div className="absolute left-0 right-0 flex flex-col items-center">
-          <div className="w-196 my-8 rounded-xl bg-amber-500 border-8 shadow-xl border-amber-600 px-4 py-2 text-center">
+          <div className="w-196 my-8 rounded-xl bg-amber-300 border-8 shadow-xl border-amber-600 px-4 py-2 text-center">
             <div>
-              <h1 className="text-white text-3xl font-semibold">{name}</h1>
-              <h1>{address}</h1>
+              <h1 id="name" className="text-black text-3xl font-semibold">{name}</h1>
+              <h1 id="elapse" style={{color: "blue", fontSize: "30px"}}>{elapse}</h1>
+              <h1 id="result" style={{color: color, fontSize: "40px"}}>{result}</h1>
             </div>
           </div>
-        </div>
-        <script src="/src/assets/js/script.js"></script>
+        </div>        
     </>
   );
 };
